25daf09d1e463570748cec3ba30d062a93158d17
Ignacio <shoik.xtazy@gmail.com>
2019-07-30 01:08:23 -0400

    send sms ok

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ const authToken  = process.env.AUTH_TOKEN;
 
 const client = require('twilio')(accountSId,authToken);
 
+client.messages.create({
+    to: process.env.MY_PHONENUMBER,
+    from: '+56937610190',
+    body: 'Hello world :D'
+})
+    .then(message => console.log(message.sid))
